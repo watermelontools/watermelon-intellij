bdb557836c2f88af728b38561d8d42c5742ecc63
baristaGeek <estebanvargas94@gmail.com>
2023-07-14 17:41:28 -0500

    Make it work in compilation

--- src/main/kotlin/com/github/baristageek/watermelonintellij/actions/GitBlameAction.kt
+++ src/main/kotlin/com/github/baristageek/watermelonintellij/actions/GitBlameAction.kt
@@ -3,22 +3,28 @@ package com.github.baristageek.watermelonintellij.actions
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.PlatformDataKeys
-import Git4Idea
+import git4idea.*
+import com.intellij.openapi.project.ProjectManager
+import com.intellij.openapi.vcs.FilePath
+import com.intellij.openapi.vfs.VirtualFile
+//import com.intellij.openapi.vcs.changes.VcsUtil
 
 class GitBlameAction : AnAction() {
-    val git = Git4Idea.getInstance();
     override fun actionPerformed(e: AnActionEvent) {
         // get current file
         val editor = e.getRequiredData(PlatformDataKeys.EDITOR)
-        val file = e.getRequiredData(PlatformDataKeys.VIRTUAL_FILE)
-
+        val file = e.getRequiredData(PlatformDataKeys.VIRTUAL_FILE) as FilePath
+        
+        // Convert VirtualFile to FilePath
+//        val filePath = VcsUtil.getFilePath(file)
 
         // Get selection line numbers
         val selectionModel = editor.selectionModel
         val startLine = editor.document.getLineNumber(selectionModel.selectionStart)
         val endLine = editor.document.getLineNumber(selectionModel.selectionEnd)
 
-        gitBlame(file.path, startLine, endLine)
+//        gitBlame(file.path, startLine, endLine)
+        gitBlame(file.path as FilePath, startLine, endLine)
 
     }
 
@@ -26,10 +32,15 @@ class GitBlameAction : AnAction() {
     //      println("selected file, start and end line: $filePath $start $end")
     //  }
 
-    fun gitBlame(filePath: String, start: Int, end: Int) {
+    fun gitBlame(filePath: FilePath, start: Int, end: Int) {
          println("selected file, start and end line: $filePath $start $end")
-//        val git = Git4Idea.getInstance()
-        val blameResult = git.getBlameForFile(filePath,start, end)
+
+        val project = ProjectManager.getInstance().getOpenProjects()[0]
+
+        val history = git4idea.history.GitFileHistory;
+//        val blameResult = history.collectHistory(project, filePath, start, end)
+        val blameResult = history.collectHistory(project, filePath, start.toString(), end.toString())
+
 
         println("Blame result: $blameResult");
     }

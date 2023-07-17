91e924054a412d9d6d3dee21584846e2d07a8fa1
baristaGeek <estebanvargas94@gmail.com>
2023-07-17 15:31:47 -0500

    Blame result printing hashes

--- src/main/kotlin/com/github/baristageek/watermelonintellij/actions/GitBlameAction.kt
+++ src/main/kotlin/com/github/baristageek/watermelonintellij/actions/GitBlameAction.kt
@@ -7,16 +7,23 @@ import git4idea.*
 import com.intellij.openapi.project.ProjectManager
 import com.intellij.openapi.vcs.FilePath
 import com.intellij.openapi.vfs.VirtualFile
-//import com.intellij.openapi.vcs.changes.VcsUtil
+import com.intellij.vcsUtil.VcsUtil;
 
 class GitBlameAction : AnAction() {
     override fun actionPerformed(e: AnActionEvent) {
         // get current file
         val editor = e.getRequiredData(PlatformDataKeys.EDITOR)
-        val file = e.getRequiredData(PlatformDataKeys.VIRTUAL_FILE) as FilePath
-        
+        val file = e.getRequiredData(PlatformDataKeys.VIRTUAL_FILE)
+        val vfPath = file.canonicalPath;
+        val weirdThing = VcsUtil.getFilePath(file.getPath())
+//        FilePath filePath = VcsUtil.getFilePath(selectedFile);
+        val filePath = VcsUtil.getFilePath(file);
+
+        println("file path after parsing: $filePath")
+
+        println("file: ${file}, file.path: ${file.path}, canonical path: $vfPath")
         // Convert VirtualFile to FilePath
-//        val filePath = VcsUtil.getFilePath(file)
+        println("weird thing: $weirdThing")
 
         // Get selection line numbers
         val selectionModel = editor.selectionModel
@@ -24,7 +31,8 @@ class GitBlameAction : AnAction() {
         val endLine = editor.document.getLineNumber(selectionModel.selectionEnd)
 
 //        gitBlame(file.path, startLine, endLine)
-        gitBlame(file.path as FilePath, startLine, endLine)
+//        gitBlame(file.path as VirtualFile, startLine, endLine)
+        gitBlame(filePath, startLine, endLine)
 
     }
 
@@ -33,13 +41,19 @@ class GitBlameAction : AnAction() {
     //  }
 
     fun gitBlame(filePath: FilePath, start: Int, end: Int) {
-         println("selected file, start and end line: $filePath $start $end")
+//         println("selected file, start and end line: $vfPath $start $end")
 
         val project = ProjectManager.getInstance().getOpenProjects()[0]
+//        println("project: $project")
 
         val history = git4idea.history.GitFileHistory;
 //        val blameResult = history.collectHistory(project, filePath, start, end)
-        val blameResult = history.collectHistory(project, filePath, start.toString(), end.toString())
+//        println("history: $history")
+
+
+        println("blame result will try to execute here: ")
+//        val blameResult = history.collectHistory(project, filePath, start.toString(), end.toString())
+        val blameResult = history.collectHistory(project, filePath)
 
 
         println("Blame result: $blameResult");

1772e4054f84b8e49f0e660ecba374f0d42af069
baristaGeek <estebanvargas94@gmail.com>
2023-07-21 17:23:24 -0500

    Render hardcoded commit hashes

--- src/main/kotlin/com/github/baristageek/watermelonintellij/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/baristageek/watermelonintellij/toolWindow/MyToolWindowFactory.kt
@@ -11,10 +11,11 @@ import com.intellij.ui.content.ContentFactory
 import com.github.baristageek.watermelonintellij.MyBundle
 import com.github.baristageek.watermelonintellij.services.MyProjectService
 import javax.swing.JButton
-
+import java.awt.Font
+import com.intellij.ui.JBColor
+import java.awt.Color
 
 class MyToolWindowFactory : ToolWindowFactory {
-
     init {
         thisLogger().warn("Don't forget to remove all non-needed sample code files with their corresponding registration entries in `plugin.xml`.")
     }
@@ -32,12 +33,30 @@ class MyToolWindowFactory : ToolWindowFactory {
         private val service = toolWindow.project.service<MyProjectService>()
 
         fun getContent() = JBPanel<JBPanel<*>>().apply {
-            val label = JBLabel(MyBundle.message("randomLabel", "?"))
+            // The scope of the commit hashes must be here
+            val commitsLabel = (JBLabel(MyBundle.message("randomLabel", "?")))
+            commitsLabel.font = commitsLabel.font.deriveFont(Font.BOLD)
+            commitsLabel.foreground = JBColor(Color(0x999999), Color(0x999999))
+            add(commitsLabel);
+            val mockedCommitHashes = arrayOf(
+                    "abc123def456789ghijklmn0pqrstuvwx",
+                    "def456789ghijklmn0pqrstuvwxabc123",
+                    "789ghijklmn0pqrstuvwxabc123def456",
+                    "ijklmn0pqrstuvwxabc123def456789g",
+                    "mn0pqrstuvwxabc123def456789ghijk",
+                    "rstuvwxabc123def456789ghijklmn0p",
+                    "vwxabc123def456789ghijklmn0pqrstu"
+            )
 
-            add(label)
+            mockedCommitHashes.forEach { commitHash ->
+                add(JBLabel(commitHash))
+            }
+//            val label = JBLabel(MyBundle.message("randomLabel", "?"))
+//
+//            add(label)
             add(JButton(MyBundle.message("shuffle")).apply {
                 addActionListener {
-                    label.text = MyBundle.message("randomLabel", service.getRandomNumber())
+                    println("random number retreived from service: " + service.getRandomNumber())
                 }
             })
         }

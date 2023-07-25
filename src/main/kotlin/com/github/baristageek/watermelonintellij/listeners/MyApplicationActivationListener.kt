bf41b06e7e49f9b7e3e637460ecd4b2400a384e8
baristaGeek <estebanvargas94@gmail.com>
2023-07-25 17:31:28 -0500

    prototype that successfully builds

--- src/main/kotlin/com/github/baristageek/watermelonintellij/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/baristageek/watermelonintellij/listeners/MyApplicationActivationListener.kt
@@ -4,9 +4,50 @@ import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.wm.IdeFrame
 
-internal class MyApplicationActivationListener : ApplicationActivationListener {
+import com.intellij.codeInsight.hint.HintManager
+import com.intellij.openapi.editor.event.EditorMouseEvent
+import com.intellij.openapi.editor.event.EditorMouseEventArea
+import com.intellij.openapi.editor.event.EditorMouseMotionListener
+import com.intellij.ui.HintHint
+import javax.swing.JLabel
+import com.intellij.ui.awt.RelativePoint
 
-    override fun applicationActivated(ideFrame: IdeFrame) {
-        thisLogger().warn("Don't forget to remove all non-needed sample code files with their corresponding registration entries in `plugin.xml`.")
+//internal class MyApplicationActivationListener : ApplicationActivationListener {
+//
+//    override fun applicationActivated(ideFrame: IdeFrame) {
+//        thisLogger().warn("Don't forget to remove all non-needed sample code files with their corresponding registration entries in `plugin.xml`.")
+//    }
+//}
+
+class MyApplicationActivationListener : EditorMouseMotionListener {
+
+    override fun mouseMoved(e: EditorMouseEvent) {
+        if(e.area == EditorMouseEventArea.LINE_NUMBERS_AREA) {
+            val line = e.editor.caretModel.logicalPosition.line
+            if(shouldShowTooltip(line)) {
+                val tooltip = createTooltip(line)
+//                HintManager.getInstance().showHint(e.editor, tooltip, HintManager.UNDER, HintManager.HIDE_BY_ANY_KEY, 0, false
+                val tooltipComponent = JLabel("My Tooltip")
+                val point = RelativePoint(e.mouseEvent.locationOnScreen)
+
+                HintManager.getInstance().showHint(
+                    tooltipComponent,
+                    point,
+                    HintManager.UNDER as Int,
+                    HintManager.HIDE_BY_ANY_KEY
+                )
+
+            }
+        }
     }
+
+    private fun shouldShowTooltip(line: Int): Boolean {
+        // Logic to check if tooltip should show on line
+        return true
+    }
+
+    private fun createTooltip(line: Int) {
+        println("hoveredLine index: $line");
+    }
+
 }

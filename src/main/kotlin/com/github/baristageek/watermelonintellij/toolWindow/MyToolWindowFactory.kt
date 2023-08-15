1f0e3f2915122007751d51d5873ea2fb0237d958
baristaGeek <estebanvargas94@gmail.com>
2023-08-15 12:08:47 -0500

    Remove old code and unused imports

--- src/main/kotlin/com/github/baristageek/watermelonintellij/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/baristageek/watermelonintellij/toolWindow/MyToolWindowFactory.kt
@@ -8,23 +8,13 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import com.github.baristageek.watermelonintellij.MyBundle
 import com.github.baristageek.watermelonintellij.services.MyProjectService
-import javax.swing.JButton
 import java.awt.Font
-import com.intellij.ui.JBColor
-import java.awt.Color
-import com.github.baristageek.watermelonintellij.actions.GitBlameAction
-import com.intellij.openapi.vcs.FilePath
-import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.actionSystem.DataContext
 import javax.swing.BoxLayout
 import javax.swing.BorderFactory
-
 import java.awt.Dimension
 import javax.swing.Box
 import javax.swing.JPanel
-
 
 class MyToolWindowFactory : ToolWindowFactory {
     init {
@@ -42,19 +32,6 @@ class MyToolWindowFactory : ToolWindowFactory {
     class MyToolWindow(toolWindow: ToolWindow) {
 
         private val service = toolWindow.project.service<MyProjectService>()
-
-//        fun getContent() = JBPanel<JBPanel<*>>().apply {
-//            val commitsLabel = JBLabel(("Git"))
-//            commitsLabel.font = commitsLabel.font.deriveFont(Font.BOLD)
-//            commitsLabel.foreground = JBColor(Color(0x999999), Color(0x999999))
-//            add(commitsLabel);
-//
-//            val commitHashes = service.getGitBlame();
-//
-//            commitHashes.forEach { commitHash ->
-//                add(JBLabel(commitHash))
-//            }
-//        }
 
         fun getContent() = JBPanel<JBPanel<*>>().apply {
 
@@ -83,11 +60,7 @@ class MyToolWindowFactory : ToolWindowFactory {
                     layout = BoxLayout(this, BoxLayout.Y_AXIS)
                 }
                 add(panel)
-
-
             }
-
-
         }
     }
 }

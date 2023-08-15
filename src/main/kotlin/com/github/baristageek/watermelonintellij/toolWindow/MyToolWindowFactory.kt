c596548f8b6db1cb199f518e4e62de12ec33264c
baristaGeek <estebanvargas94@gmail.com>
2023-08-15 12:07:44 -0500

    Improve sidebar UI

--- src/main/kotlin/com/github/baristageek/watermelonintellij/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/baristageek/watermelonintellij/toolWindow/MyToolWindowFactory.kt
@@ -18,6 +18,12 @@ import com.github.baristageek.watermelonintellij.actions.GitBlameAction
 import com.intellij.openapi.vcs.FilePath
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.DataContext
+import javax.swing.BoxLayout
+import javax.swing.BorderFactory
+
+import java.awt.Dimension
+import javax.swing.Box
+import javax.swing.JPanel
 
 
 class MyToolWindowFactory : ToolWindowFactory {
@@ -37,17 +43,51 @@ class MyToolWindowFactory : ToolWindowFactory {
 
         private val service = toolWindow.project.service<MyProjectService>()
 
+//        fun getContent() = JBPanel<JBPanel<*>>().apply {
+//            val commitsLabel = JBLabel(("Git"))
+//            commitsLabel.font = commitsLabel.font.deriveFont(Font.BOLD)
+//            commitsLabel.foreground = JBColor(Color(0x999999), Color(0x999999))
+//            add(commitsLabel);
+//
+//            val commitHashes = service.getGitBlame();
+//
+//            commitHashes.forEach { commitHash ->
+//                add(JBLabel(commitHash))
+//            }
+//        }
+
         fun getContent() = JBPanel<JBPanel<*>>().apply {
-            val commitsLabel = JBLabel(("Git"))
-            commitsLabel.font = commitsLabel.font.deriveFont(Font.BOLD)
-            commitsLabel.foreground = JBColor(Color(0x999999), Color(0x999999))
-            add(commitsLabel);
+
+            layout = BoxLayout(this, BoxLayout.Y_AXIS)
+
+            border = BorderFactory.createEmptyBorder(10, 10, 10, 10)
+
+            val titleLabel = JBLabel("Commit History").apply {
+                font = font.deriveFont(Font.BOLD, 16f)
+            }
+            add(titleLabel)
+
+            add(Box.createRigidArea(Dimension(0, 10)))
 
             val commitHashes = service.getGitBlame();
 
             commitHashes.forEach { commitHash ->
-                add(JBLabel(commitHash))
+                val commitLabel = JBLabel(commitHash).apply {
+                    font = font.deriveFont(Font.PLAIN, 14f)
+                }
+                add(commitLabel)
+
+                // Add a panel with vertical flow layout
+                // This will force each label onto a new line
+                val panel = JPanel().apply {
+                    layout = BoxLayout(this, BoxLayout.Y_AXIS)
+                }
+                add(panel)
+
+
             }
+
+
         }
     }
 }

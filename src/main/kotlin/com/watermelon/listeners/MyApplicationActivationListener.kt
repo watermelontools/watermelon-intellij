6f5c5a028e4aa5b2694d7f9afd5bc38cc2db8214
Esteban Dalel R <estebandalelr@gmail.com>
2023-08-31 11:39:07 -0500

    REmove unused loggers

--- src/main/kotlin/com/watermelon/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/watermelon/listeners/MyApplicationActivationListener.kt
@@ -1,12 +1,10 @@
 package com.watermelon.context.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
-import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.wm.IdeFrame
 
 internal class MyApplicationActivationListener : ApplicationActivationListener {
 
     override fun applicationActivated(ideFrame: IdeFrame) {
-        //thisLogger().warn("Don't forget to remove all non-needed sample code files with their corresponding registration entries in `plugin.xml`.")
     }
 }
--- src/main/kotlin/com/watermelon/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/watermelon/toolWindow/MyToolWindowFactory.kt
@@ -1,7 +1,6 @@
 package com.watermelon.context.toolWindow
 
 import com.intellij.openapi.components.service
-import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
@@ -18,7 +17,6 @@ import javax.swing.JPanel
 
 class MyToolWindowFactory : ToolWindowFactory {
     init {
-        //  thisLogger().warn("Don't forget to remove all non-needed sample code files with their corresponding registration entries in `plugin.xml`.")
     }
 
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {

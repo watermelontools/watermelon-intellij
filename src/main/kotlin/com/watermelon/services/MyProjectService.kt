59c61b86a6e62b8e6841eab24269faded55ebd0e
Esteban Dalel R <estebandalelr@gmail.com>
2023-08-31 17:26:50 -0500

    Fix blame getter

--- src/main/kotlin/com/watermelon/services/MyProjectService.kt
+++ src/main/kotlin/com/watermelon/services/MyProjectService.kt
@@ -7,43 +7,62 @@ import com.intellij.vcsUtil.VcsUtil
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.fileEditor.FileEditorManager
+import git4idea.commands.Git
+import git4idea.commands.GitCommand
+import git4idea.commands.GitCommandResult
+import git4idea.commands.GitLineHandler
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
-
+    // Get the file being currently edited
+    private val project = ProjectManager.getInstance().openProjects[0]
+    private val editor: Editor = FileEditorManager.getInstance(project).selectedTextEditor!!
+    private val document = editor.document
+    private val file = FileDocumentManager.getInstance().getFile(document)
+    private val directory = file?.parent
+    private val filePath = VcsUtil.getFilePath(file!!);
     fun getGitBlame(): ArrayList<String> {
-        // Get the file being currently edited
-        val project = ProjectManager.getInstance().openProjects[0]
-        val editor: Editor = FileEditorManager.getInstance(project).selectedTextEditor!!
-        val document = editor.document
-        val file = FileDocumentManager.getInstance().getFile(document!!)
-
-        // Get file path
-        val filePath = VcsUtil.getFilePath(file!!);
-
-        // Get selection line numbers
-        val selectionModel = editor.selectionModel
-        val startLine = editor.document.getLineNumber(selectionModel.selectionStart)
-        val endLine = editor.document.getLineNumber(selectionModel.selectionEnd)
-        println("startLine $startLine")
-        println("endLine $endLine")
-        val history = git4idea.history.GitFileHistory;
-
-        val blameResult = history.collectHistory(project, filePath)
-        val commitHashes = ArrayList<String>()
         val commitMessages = ArrayList<String>()
-        blameResult.forEach {
-            val commitMessageWithAuthor = "${it.author}: ${it.commitMessage}"
-            val stringElement = it.toString().split(":")[1]
-            commitHashes.add(stringElement)
-            commitMessages.add(commitMessageWithAuthor)
+        // print changes for current revision
+        val blameRun = directory?.let {
+            git4idea.commands.GitLineHandler(
+                project, it, GitCommand.BLAME,
+            ).apply {
+                addParameters("$filePath")
+            }
         }
 
-        // TODO: Run a blame range correctly
-        // val rangeBlame = blameResult.slice(start..end);
-        // println("rangeblame: $rangeBlame");
+        fun runCommand(lineHandler: GitLineHandler): GitCommandResult {
+            return Git.getInstance().runCommand(lineHandler)
+        }
+
+        val blameCommandResponse = blameRun?.let { runCommand(it) }
+        println("blameCommandResponse: $blameCommandResponse");
 
         return (commitMessages);
     }
 
+    fun getPartialGitBlame(startLine: Int, endLine: Int): ArrayList<String> {
+        // Get the file being currently edited
+
+        val commitMessages = ArrayList<String>()
+        // print changes for current revision
+        val blameRun = directory?.let {
+            git4idea.commands.GitLineHandler(
+                project, it, GitCommand.BLAME,
+            ).apply {
+                addParameters("-L$startLine,$endLine")
+                addParameters("$filePath")
+            }
+        }
+
+        fun runCommand(lineHandler: GitLineHandler): GitCommandResult {
+            return Git.getInstance().runCommand(lineHandler)
+        }
+
+        val blameCommandResponse = blameRun?.let { runCommand(it) }
+        println("blameCommandResponse: $blameCommandResponse");
+
+        return (commitMessages);
+    }
 }
